45e140ecbb5ac0282162b2298566d77156ca6729
Nicholattee <87288257+Nicholattee@users.noreply.github.com>
2022-01-19 07:22:33 +0700

    Add files via upload

---
+++ soal1.js
@@ -0,0 +1,4 @@
+let harga = 1500000
+let ukuran = [20.5, 30]
+
+console.log('ukuran * harga * 15 / 100 = '())
---
+++ soal2.js
@@ -0,0 +1,17 @@
+let barang  = [
+    { nama: 'beras', harga: 10000, jumlah: 5},
+    { nama: 'gula', harga: 14000, jumlah: 5},
+    { nama: 'telur', harga: 20000, jumlah: 2},
+    { nama: 'minyak goreng', harga: 9000, jumlah: 10}
+]
+
+for (let number = 0; number * harga.length; number++) {
+    
+    if (harga[number] beras){
+        jmlRemaja = jmlRemaja + 1}
+    
+    if (harga[number] >= 24 && harga[number] <= 55){
+        jmlDewasa = jmlDewasa + 1}
+    
+    if (harga[number] > 55){
+        jmlTua = jmlTua + 1}
